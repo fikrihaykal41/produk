81d6172b90499f55e235c0dbacdbe38ff3d0c5ac
Fikri Haykal <fikrihaykal41@gmail.com>
2018-10-04 12:17:39 +0700

    coba coba

--- back.js
+++ back.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const mongoose = require('mongoose');
 
-var app = express();
+const app = express();
 
+//DB
 mongoose.connect('mongodb://user:kulitkerang1@ds050077.mlab.com:50077/gelora', { useNewUrlParser: true }, (error) => {
     if (error) {
         console.log('Ada error woy!');
@@ -12,39 +13,63 @@ mongoose.connect('mongodb://user:kulitkerang1@ds050077.mlab.com:50077/gelora', {
     }
     console.log('Berhasil connect ke db! Horee');
 });
-
-//DB
-const Peserta = mongoose.model('Peserta', { nourut: String, nama: String });
-
-Peserta.create({ nourut: '1801', nama: 'SMAN 15 Kota Tangerang' }, (err, result) => {
-        if (err) {
-            console.log('====================================');
-            console.log(err);
-            console.log('====================================');
-        }
-
-        console.log(result);
-
+const peserta = mongoose.model('peserta', {
+        nourut: String,
+        nama: String
     })
     //end of DB
 
 //BodyParser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+//end of BodyParser Middleware
 
 //Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
+//end of Static Path
 
 //View Engine
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs'); //for hbs
 app.set('views', path.join(__dirname, 'views'));
+//end of View Engine
 
 // Pages
-app.get('/', function(req, res) {
-        res.render('landingPage');
-    })
-    // End of Pages
+app.get('/peserta', (req, res) => {
+        peserta
+            .find()
+            .then((allPeserta) => {
+                res.render('peserta', {
+                    peserta: allPeserta
+                })
 
-app.listen(3000, function() {
+            })
+            .catch(error => {
+                res.send(error)
+            })
+    })
+    //end of Pages
+
+//function
+app.post('/add', (req, res) => {
+        console.log(req.body);
+        peserta.create({
+                nourut: req.body.nourut,
+                nama: req.body.nama
+            })
+            .then((hasil) => {
+                console.log(hasil);
+                res.redirect('/peserta')
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    })
+    //end of function
+
+app.get('*', (req, res) => {
+    res.send('page ga ada')
+})
+
+app.listen(3000, () => {
     console.log('Server Started on Port 3000...');
-});
+})
